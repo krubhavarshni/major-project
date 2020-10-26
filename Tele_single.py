3a5ad70a59270281d627995c7406e967f9fb52d0
krubhavarshni <73431526+krubhavarshni@users.noreply.github.com>
2020-10-26 17:00:00 +0530

    Update Tele_single.py

--- Tele_single.py
+++ Tele_single.py
@@ -3,57 +3,54 @@ from telegram.ext import Updater, CommandHandler, Filters, MessageHandler
 import requests
 import os
 
+# x=ADAFRUIT_IO_USERNAME
+# y=ADAFRUIT_IO_KEY
+x = os.getenv('x')
+y = os.getenv('y')
+Telegram_Token= os.getenv('Telegram_Token')
 
-# a=ADAFRUIT_IO_USERNAME
-# b=ADAFRUIT_IO_KEY
-a = os.getenv('a')
-b = os.getenv('b')
-T_T = os.getenv('T_T')
-
-
-def Lets_go(bot, update):
-    bot.send_message(chat_id = update.effective_chat.id, text="YOKOSOO !!")
-    bot.send_message(chat_id = update.effective_chat.id, text=" Type 'Turn on the light' if you want to turn on the light or type 'Turn off the light' if you want to turn off the light")
+def start(bot, update):
+    bot.send_message(chat_id = update.effective_chat.id, text="Welcome!")
+    bot.send_message(chat_id = update.effective_chat.id, text="if you want to turn on the light then type 'Turn on the light' or if you want to turn off the light then type in 'Turn off the light'")
     
-def err_msg(bot, update):
+def wrong_message(bot, update):
     bot.send_message(chat_id=update.effective_chat.id, text="Oops! I couldn't recognize.Please try again!!")
 
 def send_data_to_adafruit(value1):
     value=Data(value=value1)
-    value_send=aio.create_data('light control',value)
+    value_send=aio.create_data('telebot',value)
     
-def Lights_on(bot, update):
+def turn_on_light(bot, update):
   chat_id = update.message.chat_id
   bot.send_message(chat_id, text="Turning on the light")
-  bot.send_photo(chat_id, photo='https://www.pinclipart.com/picdir/big/348-3486135_png-file-svg-turn-off-lights-icon-clipart.png')
+  bot.send_photo(chat_id, photo='https://web02.splash.abc.net.au/splash-image-servlet/mvcservlet/imageServlet/profile2/ABCTEC027')
   send_data_to_adafruit(1)
   
-def Lights_off(bot, update):
+def turn_off_light(bot, update):
   chat_id = update.message.chat_id
   bot.send_message(chat_id, text="Turning off the light")
-  bot.send_photo(chat_id=update.effective_chat.id,photo='https://cdn4.iconfinder.com/data/icons/ui-controls-miscellaneous/32/interface-controls-light-bulb-off-512.png')
+  bot.send_photo(chat_id=update.effective_chat.id,photo='https://ak.picdn.net/shutterstock/videos/1697878/thumb/5.jpg')
   send_data_to_adafruit(0)
   
 def text_given(bot, update):
   text = update.message.text
   if text == 'Start':
-   Lets_go(bot,update)
+    start(bot,update)
   elif text == 'Turn on the light':
-    Lights_on(bot,update)
+    turn_on_light(bot,update)
   elif text == 'Turn off the light':
-    Lights_off(bot,update)
+    turn_off_light(bot,update)
   else:
-    err_msg(bot,update)
+    wrong_message(bot,update)
     
-aio = Client(a,b)  
-u = Updater(T_T)
+aio = Client(x,y)  
+u = Updater(Telegram_Token)
 dp = u.dispatcher
-dp.add_handler(CommandHandler('turnonthelight',Lights_on))
-dp.add_handler(CommandHandler('turnoffthelight',Lights_off))
-dp.add_handler(CommandHandler('start', Lets_go))
-dp.add_handler(MessageHandler(Filters.command, err_msg))
+dp.add_handler(CommandHandler('turnonthelight',turn_on_light))
+dp.add_handler(CommandHandler('turnonthelight',turn_off_light))
+dp.add_handler(CommandHandler('start', start))
+dp.add_handler(MessageHandler(Filters.command, wrong_message))
 dp.add_handler(MessageHandler(Filters.text, text_given))
-
 
 u.start_polling()
 u.idle()
